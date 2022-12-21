350e27ad66c2091606276e1a463b359a8b174dc3
Natalia <kornchak365@gmail.com>
2022-12-21 13:58:48 +0200

    initial

---
+++ main.js
@@ -0,0 +1,17 @@
+// var fisrstIndex=0;
+// function automaticSlide(){
+// setTimeout(automaticSlide, 25000);
+// var pics;
+// const img=document.querySelectorAll("#img-slide");
+// for(pics=0;pics<img.length;pics++){
+//     img[pics].style.display="none";
+// }
+// fisrstIndex++;
+// if(fisrstIndex>img.length){
+//     fisrstIndex=1;
+// }
+// img[fisrstIndex-1].style.display="block";
+
+
+// }
+// automaticSlide();
